8ed4054002b68227e0989859cc41adfc0efeaaf9
Sebastián Galiano <sgaliano@gmail.com>
2020-05-16 11:41:45 -0300

    Bump dependencies

--- src/mappings.ts
+++ src/mappings.ts
@@ -1,11 +1,12 @@
-import { EthereumBlock } from '@graphprotocol/graph-ts'
+import { ethereum } from '@graphprotocol/graph-ts'
 
-import { BlockGasUsage } from '../generated/schema'
+import { Block } from '../generated/schema'
 
-export function handleBlock(block: EthereumBlock): void {
-  let entity = new BlockGasUsage(block.number.toString())
-  entity.blockNumber = block.number
-  entity.blockSize = block.size
+export function handleBlock(block: ethereum.Block): void {
+  let entity = new Block(block.number.toString())
+  entity.number = block.number
+  entity.size = block.size
+
   entity.gasUsed = block.gasUsed
   entity.gasLimit = block.gasLimit
   entity.timestamp = block.timestamp
